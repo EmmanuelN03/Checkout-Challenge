087a76ea80d98dd03902cf21d338418cb1038aac
EmmanuelN03 <67364000+EmmanuelN03@users.noreply.github.com>
2020-07-01 23:52:38 +0400

    Add files via upload

---
+++ Project/PaymentGateway/Log/Interface/ILog.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PaymentGateway.Interface
+{
+    public interface ILog
+    {
+        public void Information(string message);
+        public void Warning(string message);
+        public void Debug(string message);
+        public void Error(string message);
+
+    }
+}
